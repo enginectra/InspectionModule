6628315e1cdd710bf70a1f61a265eddf0d73e404
Chinenye <luvchiny@yahoo.com>
2020-11-17 15:54:39 -0700

    add to main

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
 class PagesController < ApplicationController
-<<<<<<< HEAD
-
-def index #home is the action
-=======
 def index
 
->>>>>>> 49ca2bb31dcd3aa18652bc9984eaaaf4b48fef81
 end
 
 def about
