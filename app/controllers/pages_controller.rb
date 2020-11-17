7553e533340ae5b36d9b94f1c92e7db159b4d2d5
basel abdul-aziz <basel.abdulazizz@gmail.com>
2020-11-17 12:03:14 -0700

    Added index,admin,engineer pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+def index 
+end
+
+def about
+end
+
+def engineer
+end
+
+def admin
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  get 'home/index'
+  root 'pages#index'
+  get '/about', to: "pages#about"
+  get '/engineer', to: "pages#engineer"
+  get '/admin', to: "pages#admin"
+  get '/index', to: "pages#index"
+
+  
+
+
 end
