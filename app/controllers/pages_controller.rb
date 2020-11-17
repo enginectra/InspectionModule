cc068dda9d33e8e9b63a0b5bb4d47696f753296b
Chinenye <luvchiny@yahoo.com>
2020-11-17 10:49:16 -0700

    added navigation bar to the home page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+
+    def home #home is the action
+
+    end
+
+    def about
+
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # This takes you to the default page instead of showing yay you are on rails. application is the 
+  # application controller while hello is the hello function within the controller
+
+ root "pages#home" 
+ get "/about", to: "pages#about"  #grab this route and send it to controller action
 end
