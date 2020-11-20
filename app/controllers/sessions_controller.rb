de324352ec72b3d3d73175a9c492f07fbc59440e
basel abdul-aziz <basel.abdulazizz@gmail.com>
2020-11-20 16:56:05 -0700

    user authentications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,18 @@
 class SessionsController < ActionController::Base
-    protect_from_forgery with: :exception
-  end
+    def create 
+      customer = Customer.find_by(email: login_params[:email])
+      if customer && customer.authenticate(login_params[:password_hash])
+        session[:customer_id] = customer.id 
+        redirect_to '/index'
+      else
+        flash[:login_errors] = ['invalid credentials']
+        redirect_to '/signinc'
+      end
+    end
+
+
+    private
+    def login_params
+      params.require(:login).permit(:email, :password)
+    end
+end
